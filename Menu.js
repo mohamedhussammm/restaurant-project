6081c3d9b4be7274149bc8e34a012b3abbeede00
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2023-07-21 23:25:12 +0300

    hover buttons

--- Menu.js
+++ Menu.js
@@ -1,22 +1,7 @@
-//  Slide Show
-var myIndex = 0;
-carousel();
-function carousel() {
-var i;
- var x = document.getElementsByClassName("mySlides");
-for (i = 0; i < x.length; i++) {
-x[i].style.display = "none";  
-}
-myIndex++;
-if (myIndex > x.length) {
-myIndex = 1
-}    
-x[myIndex-1].style.display = "block";  
-setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+
 let current="ordd";
-            function change_view(target){
-                document.getElementById(current).style.display='none';
-                document.getElementById(target).style.display='block';
-                current=target;
-            }
+function change_view(target){
+    document.getElementById(current).style.display='none';
+    document.getElementById(target).style.display='block';
+    current=target;
+}
